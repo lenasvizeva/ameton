2454248d08ac71380b393d721b3328306b988b1e
elena <kaiout@mail.ru>
2024-01-11 23:14:50 +0400

    adaptive

--- src/assets/js/slider.js
+++ src/assets/js/slider.js
@@ -1,5 +1,16 @@
 $('.slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    infinite: false
+    infinite: false,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3
+        }
+    }, {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1
+        }
+    }]
 })
